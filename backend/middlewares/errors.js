60299248943dbf364852d63fe9adfe11ce6b8bf8
SherlynDuan <118089268+SherlynDuan@users.noreply.github.com>
2023-08-02 20:54:30 +1200

    more error handlings

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -18,9 +18,22 @@ module.exports =(err, req, res, next) => {
             let error={...err}
             error.message=err.message;
 
+             //wrong mongoose object id
+
+            if (err.name==="CastError") {
+                const message=`Resource not found. Invalid :${err.path}`
+                error=new ErrorHandler(message, 400)
+            }
+
+            //handle mongoose validation
+            if (err.name==="ValidationError") {
+                const message=Object.values(err.errors).map(value=>value.message);
+                error=new ErrorHandler(message, 400)
+            }
+
             res.status(err.statusCode).json({
                 success:false,
-                message:err.message || "internal server error"
+                message:error.message || "internal server error"
             })
     }
 }

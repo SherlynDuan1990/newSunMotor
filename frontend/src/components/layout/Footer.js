c8961032a1d54dcee1a8f3a05945654d1a37afb4
SherlynDuan <118089268+SherlynDuan@users.noreply.github.com>
2023-08-03 22:45:11 +1200

    fix bugs

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,42 +1,50 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 
 const Footer = () => {
   return (
-    <footer className="footer" style={{ backgroundColor: '#134883' }}>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <h4 className="footer-heading">Contact Us</h4>
-            <p className="footer-text">
-              Henry He Sales Manager <br />
-              Phone: <span className="footer-emphasis">07 8467888, 022 6989700</span> <br />
-              Email: <span className="footer-emphasis">henryaax@gmail.com</span>
-            </p>
-          </div>
-          <div className="col-md-4">
-            <h4 className="footer-heading">Find Us</h4>
-            <p className="footer-text">
-              Phone: <span className="footer-emphasis">07 8467888, 022 6989700</span> <br />
-              Email: <span className="footer-emphasis">henryaax@gmail.com</span> <br />
-              Address: <span className="footer-emphasis">718b Te Rapa Road, Te Rapa, Hamilton</span>
-            </p>
-          </div>
-          <div className="col-md-4">
-            <h4 className="footer-heading">Opening Hours</h4>
-            <p className="footer-text">
-              Mon-Fri: <span className="footer-emphasis">9:00-5:00</span> <br />
-              Sat: <span className="footer-emphasis">9:00-12:00</span> <br />
-              Sun: <span className="footer-emphasis">Closed</span>
-            </p>
-            <p>
-              <a href="/dealer-login" className="footer-link">Dealer Login</a>
-            </p>
+    <Fragment>
+      <footer className="footer" style={{ backgroundColor: '#134883' }}>
+
+        <div className="container">
+          <div className="row">
+
+            <div className="col-md-4">
+              <h4 className="footer-heading">Contact Us</h4>
+              <p className="footer-text">
+                Henry He Sales Manager <br />
+                Phone: <span className="footer-emphasis">07 8467888, 022 6989700</span> <br />
+                Email: <span className="footer-emphasis">henryaax@gmail.com</span>
+              </p>
+            </div>
+
+            <div className="col-md-4">
+              <h4 className="footer-heading">Find Us</h4>
+              <p className="footer-text">
+                Phone: <span className="footer-emphasis">07 8467888, 022 6989700</span> <br />
+                Email: <span className="footer-emphasis">henryaax@gmail.com</span> <br />
+                Address: <span className="footer-emphasis">718b Te Rapa Road, Te Rapa, Hamilton</span>
+              </p>
+            </div>
+
+            <div className="col-md-4">
+              <h4 className="footer-heading">Opening Hours</h4>
+              <p className="footer-text">
+                Mon-Fri: <span className="footer-emphasis">9:00-5:00</span> <br />
+                Sat: <span className="footer-emphasis">9:00-12:00</span> <br />
+                Sun: <span className="footer-emphasis">Closed</span>
+              </p>
+              <p>
+                <a href="/dealer-login" className="footer-link">Dealer Login</a>
+              </p>
+            </div>
+
           </div>
         </div>
-      </div>
-    </footer>
-  );
-};
+      </footer>
+    </Fragment>
 
-export default Footer;
+  )
+}
+
+export default Footer

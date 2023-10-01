f1e88eda65834ee25a7d4da31b5157f395eecb0f
SherlynDuan <118089268+SherlynDuan@users.noreply.github.com>
2023-10-01 17:24:25 +1300

    update footer with user data

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -1,44 +1,59 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useAlert } from 'react-alert';
 import { logout, clearErrors } from '../../actions/userActions';
-
-
+import axios from 'axios';
 
 const Footer = () => {
-
   const alert = useAlert();
   const dispatch = useDispatch();
-  // const { user, loading } = useSelector((state) => state.auth);  due to the user state is initially being loaded when the page refreshes, so using user state as a condition to display different contents for different interfaces is not working as i expected
   const userFromLocalStorage = JSON.parse(localStorage.getItem('user'));
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    // Fetch user data when the component mounts
+    async function fetchUserData() {
+      try {
+        const res = await axios.get('/api/v1/me'); // Replace with your API endpoint
+        if (res.data.success) {
+          const userData = res.data.user;
+          setUserData(userData);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        alert.error('Failed to fetch user data');
+      }
+    }
+
+    if (userFromLocalStorage) {
+      fetchUserData();
+    }
+  }, [alert, userFromLocalStorage]);
 
   const handleLogout = () => {
     dispatch(logout());
   };
 
-
   return (
     <Fragment>
       <footer className="footer" style={{ backgroundColor: '#134883' }}>
-
         <div className="container">
           <div className="row">
-
             <div className="col-md-4">
               <h4 className="footer-heading">Contact Us</h4>
               <p className="footer-text">
                 Henry He Sales Manager <br />
-                Phone: <span className="footer-emphasis">07 8467888, 022 6989700</span> <br />
-                Email: <span className="footer-emphasis">henryaax@gmail.com</span>
+                Phone: <span className="footer-emphasis">{userData ? userData.phone : 'Loading...'}</span> <br />
+                Email: <span className="footer-emphasis">{userData ? userData.email : 'Loading...'}</span>
               </p>
             </div>
 
             <div className="col-md-4">
               <h4 className="footer-heading">Find Us</h4>
               <p className="footer-text">
-                Phone: <span className="footer-emphasis">07 8467888, 022 6989700</span> <br />
-                Email: <span className="footer-emphasis">henryaax@gmail.com</span> <br />
+                Phone: <span className="footer-emphasis">{userData ? userData.phone : 'Loading...'}</span> <br />
+                Email: <span className="footer-emphasis">{userData ? userData.email : 'Loading...'}</span> <br />
                 Address: <span className="footer-emphasis">718b Te Rapa Road, Te Rapa, Hamilton</span>
               </p>
             </div>
@@ -51,28 +66,24 @@ const Footer = () => {
                 Sun: <span className="footer-emphasis">Closed</span>
               </p>
               <p>
-
-              {!userFromLocalStorage ? (
-                <Link to="/login" className="footer-link" style={{ color: 'white', paddingLeft: '5px', fontSize: '22px' }}>
-                  Dealer Login
-                </Link>
-              ) : (
-                <div>
-                  <Link onClick={handleLogout} style={{ color: 'white', paddingLeft: '5px', fontSize: '22px' }} to="/">
-                    Log out
+                {!userFromLocalStorage ? (
+                  <Link to="/login" className="footer-link" style={{ color: 'white', paddingLeft: '5px', fontSize: '22px' }}>
+                    Dealer Login
                   </Link>
-                </div>
-              )}     
-              
+                ) : (
+                  <div>
+                    <Link onClick={handleLogout} style={{ color: 'white', paddingLeft: '5px', fontSize: '22px' }} to="/">
+                      Log out
+                    </Link>
+                  </div>
+                )}
               </p>
             </div>
-
           </div>
         </div>
       </footer>
     </Fragment>
+  );
+};
 
-  )
-}
-
-export default Footer
+export default Footer;

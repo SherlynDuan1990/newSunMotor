4c415af7870cc068e1ac46646855ea395fe75786
SherlynDuan <118089268+SherlynDuan@users.noreply.github.com>
2023-10-01 17:22:07 +1300

    update header with user data

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,50 +1,68 @@
-import React, {Fragment} from 'react'
-import "../../App.css"
-
-import { useDispatch, useSelector } from 'react-redux';
+import React, { Fragment, useEffect, useState } from 'react';
 import { useAlert } from 'react-alert';
-import { Link } from 'react-router-dom';
-
-
-
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 
 const Header = () => {
   const alert = useAlert();
-  const dispatch = useDispatch();
-  // const { user, loading } = useSelector((state) => state.auth);  due to the user state is initially being loaded when the page refreshes, so using user state as a condition to display different contents for different interfaces is not working as i expected
   const userFromLocalStorage = JSON.parse(localStorage.getItem('user'));
+  const [userData, setUserData] = useState(null);
 
+  useEffect(() => {
+    // Fetch user data when the component mounts
+    async function fetchUserData() {
+      try {
+        const res = await axios.get('/api/v1/me'); // Replace with your API endpoint
+        if (res.data.success) {
+          const userData = res.data.user;
+          setUserData(userData);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        alert.error('Failed to fetch user data');
+      }
+    }
+
+    if (userFromLocalStorage) {
+      fetchUserData();
+    }
+  }, [alert, userFromLocalStorage]);
 
   return (
     <Fragment>
-        <nav className="navbar row">
-            <div className="col-12 col-md-3">
-                <div className="navbar-brand">
-                <img  className="logoImg" src="/images/logo.png" />
-                </div>
-            </div>
+      <nav className="navbar row">
+        <div className="col-12 col-md-3">
+          <div className="navbar-brand">
+            <img className="logoImg" src="/images/logo.png" alt="Logo" />
+          </div>
+        </div>
 
-           
+        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+          <i style={{ fontSize: '100%', verticalAlign: 'middle' }} className="material-icons">phone</i>
+          {userData ? (
+            <span>Call us: {userData.phone}</span>
+          ) : (
+            <span>Call us: Loading...</span>
+          )}
+          <br />
+          <i style={{ fontSize: '100%', verticalAlign: 'middle' }} className="material-icons">email</i>
+          {userData ? (
+            <span>Email: {userData.email}</span>
+          ) : (
+            <span>Email: Loading...</span>
+          )}
+        </div>
 
-            <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
-                <i style={{ fontSize: '100%', verticalAlign: 'middle' }} className="material-icons">phone</i> Call us: 07 8467888
-                <br />
-                <i style={{ fontSize: '100%', verticalAlign: 'middle' }}className="material-icons">email</i> Email: henryaax@gmail.com
-            </div>
-
-            {userFromLocalStorage && (
-              <div className="ml-4 dropdown d-inline">
-              <a href="/admin/account" className="btn dropdown-toggle text-white" role="button">
-                <i className="material-icons" style={{ color: "#4E7299" }}>account_circle</i>
-              </a>
-            </div>
-            
-            )}
-          </nav>  
-
-            
+        {userFromLocalStorage && (
+          <div className="ml-4 dropdown d-inline">
+            <a href="/admin/account" className="btn dropdown-toggle text-white" role="button">
+              <i className="material-icons" style={{ color: "#4E7299" }}>account_circle</i>
+            </a>
+          </div>
+        )}
+      </nav>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
